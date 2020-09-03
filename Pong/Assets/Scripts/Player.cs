e0d11be86c865b9323e2074446659b66ab58b8eb
shivdiv1234567 <61931222+shivdiv1234567@users.noreply.github.com>
2020-09-03 18:06:17 +0530

    Pong
    Pong Player

---
+++ Pong/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 10.0f;
+    private Rigidbody2D rb;
+    private float Ymove;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ymove = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(0, Ymove * speed * Time.deltaTime, 0);
+        Mathf.Clamp(transform.position.y, -4, 4);
+    }
+}
---
+++ Pong/Assets/Scripts/ball.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ball : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 force;
+    public float speed = 5f;
+    public TMP_Text scoreText;
+    private int Score = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        force = Vector2.one.normalized;
+        
+        rb.AddForce(force * speed, ForceMode2D.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Score++;
+            scoreText.text = Score.ToString();
+            Debug.Log(Score);
+        }
+    }
+}
